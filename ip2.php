db7cadec1f695aa361573210b22b2700c93f3d2a
Darren-Steve <50610753+darren2025@users.noreply.github.com>
2019-11-09 21:35:56 +0800

    增加一下注释

--- ip2.php
+++ ip2.php
@@ -1,9 +1,11 @@
 <?php
-
+	// 取得远程的数据
  $data = file_get_contents('https://www.kuaidaili.com/free/intr/1/');
+ // 匹配一共有多少页
         preg_match('/\d{4}(?=<\/a><\/li><li>页<\/li><\/ul>)/', $data, $pega);
+        // 获取总页数
         $oag = $pega[0];
-
+        // 循环遍历
         for ($i=1; $i <=$oag ; $i++) {
             // 延时一秒
             sleep(1);
@@ -17,7 +19,6 @@
             foreach ($tr[0] as  $value) {
                 // 第三次 匹配 td
                 preg_match_all('/<td[.\s\S]*?>[.\s\S]*?<\/td>/', $value, $td);
-
                 // 匹配IP
                 preg_match('/<td[^>]+>(.*?)<\/td>/', $td[0][0], $ip);
                 // 匹配端口 
@@ -25,7 +26,7 @@
 
                 // 组装数据,写入
                 $str = $ip[1] . ':' . $port[1];
-
+                // 写入数据,最后换行
                 file_put_contents('./2.txt', $str, FILE_APPEND);
             }
         }
